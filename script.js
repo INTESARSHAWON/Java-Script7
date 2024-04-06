91ede698b9bd0935bfb135f96e56aca30ea19102
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-06 20:36:32 +0600

    how to get ID

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log("hihi");
-alert("hi DOM");
+//alert("hi DOM");
 console.dir(document);
 console.dir(document.body);
 // console.dir() diye js er moddhe html ki ache seta dekhe
+
+
+//id-> getElementById()
+let myParagrah = document.getElementById("paragraph");
+console.dir(myParagrah);
